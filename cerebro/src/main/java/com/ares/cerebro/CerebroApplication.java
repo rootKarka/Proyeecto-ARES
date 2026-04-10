a38be0ee28bb38f0bbe8c6a9de8b74d135aaf7c4
Jonel Aron <jonel.villanueva@tecsup.edu.pe>
2026-04-09 23:05:23 -0500

    Spring Boot: endpoint creado sobre analizar funcionando

---
+++ cerebro/src/main/java/com/ares/cerebro/CerebroApplication.java
@@ -0,0 +1,13 @@
+package com.ares.cerebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebroApplication.class, args);
+	}
+
+}
