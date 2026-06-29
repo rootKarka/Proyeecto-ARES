431e08a130d3747fa02caa50dd02c3e2adc9daf4
RootKarka <patrick.pajares@tecsup.edu.pe>
2026-06-29 16:23:13 -0500

    Toy  cansao jefe

--- ares-frontend/src/config/api.js
+++ ares-frontend/src/config/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://proyeecto-ares.onrender.com/api/";
+const BASE_URL = "https://proyeecto-ares.onrender.com/api";
 
 export const API = {
   robots:        `${BASE_URL}/robots/`,
