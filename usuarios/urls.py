f59f42a1c09363f7ddca0da940f5cb1140009470
RootKarka <patrick.pajares@tecsup.edu.pe>
2026-06-29 18:51:58 -0500

    Fix duplicated API routes

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,12 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet
 
 router = DefaultRouter()
-# El primer parámetro 'usuarios' será el prefijo de la URL.
 router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 
 urlpatterns = [
     path('', include(router.urls)),
-
-    # Al meter 'api/' e incluir router.urls, todas tus rutas empezarán con /api/
-    path('api/', include(router.urls)),
 ]
